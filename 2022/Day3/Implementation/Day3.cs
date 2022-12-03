d0da1e8d1fb7fcdcd9f274bc45971c50a820dc54
sam <samuelsalam815@gmail.com>
2022-12-03 06:56:08 +0000

    Rewritten Tests Implemented main functionality

--- 2022/Day3/Implementation/Day3.cs
+++ 2022/Day3/Implementation/Day3.cs
@@ -1,7 +1,75 @@
-﻿internal class Day3
+﻿using System.Collections;
+
+public class Day3
 {
+    private static int ToPriority(char item) => item switch
+    {
+        (>= 'a' and <= 'z') => item - 'a' + 1,
+        (>= 'A' and <= 'Z') => item - 'A' + 27,
+        _ => throw new Exception($"Char value '{item}' has no defined priority")
+    };
+
+    private static char FromPriority(int priority) => priority switch
+    {
+        (>= 1 and <= 26) => (char)('a' + priority - 1),
+        (>= 27 and <= 52) => (char)('A' + priority - 27),
+        _ => throw new Exception($"Priority value '{priority}' has no defined character")
+    };
+
+    // Helper method for debugging
+    private static string GetMaskedCharacters(BitArray mask)
+    {
+        string result = "";
+        for(int i = 0; i < 52; i++)
+        {
+            if (mask.Get(i))
+            {
+                result += FromPriority(i+1);
+            }
+        }
+
+        return result;
+    }
+
+    public static char FindRucksackOutlier(string rucksack)
+    {
+        // Use bits to indicate the presence of an item in a compartment
+        // 1st bit is 'a', 2nd bit is 'b' ...
+        BitArray firstCompartmentMask = new(52);
+        BitArray secondCompartmentMask = new(52);
+
+        // Place all items into their comparments
+        for(int i = 0; i < rucksack.Length; i++)
+        {
+            if(i+1 < rucksack.Length / 2)
+            {
+                firstCompartmentMask.Set(ToPriority(rucksack[i]) - 1, true);
+            }
+            else
+            {
+                secondCompartmentMask.Set(ToPriority(rucksack[i]) - 1, true);
+            }
+        }
+        
+        // Take the intersection of the types of items present in each compartment
+        // Exactly one will be in both components. Return this character
+        BitArray overlappingItems = new(firstCompartmentMask);
+        overlappingItems.And(secondCompartmentMask);
+            
+        for(int i = 0; i < overlappingItems.Length; i++)
+        {
+            if (overlappingItems.Get(i))
+            {
+                return FromPriority(i + 1);
+            }
+        }
+
+        throw new Exception("Unexpected failure to find outlier");
+    }
+
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
     }
 }
+
--- 2022/Day3/Tests/TestsDay3.cs
+++ 2022/Day3/Tests/TestsDay3.cs
@@ -1,23 +1,23 @@
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-
 namespace Tests
 {
     [TestClass]
     public class TestsDay3
     {
         [TestMethod]
-        [DataRow("vJrwpWtwJgWrhcsFMMfFFhFp", "vJrwpWtwJgWr", "hcsFMMfFFhFp")]
-        [DataRow("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL")]
-        [DataRow("PmmdzqPrVvPwwTWBwg", "PmmdzqPrV", "vPwwTWBwg")]
-        public void SplitRucksack_ReturnsCorrectCompartments(string rucksack, string expectedFirstCompartment, string expectedSecondCompartment)
+        [DataRow("vJrwpWtwJgWrhcsFMMfFFhFp", 'p')]
+        [DataRow("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", 'L')]
+        [DataRow("PmmdzqPrVvPwwTWBwg", 'P')]
+        [DataRow("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", 'v')]
+        [DataRow("ttgJtRGJQctTZtZT", 't')]
+        [DataRow("CrZsJsPPZsGzwwsLwLmpwMDw", 's')]
+        public void FindRucksackOutlier_ReturnsCorrectCompartments(string rucksack, char expectedOutlier)
         {
             // Arrange
             // Act
-            IEnumerable<string> actualCompartments = Program.SplitRuckSack(rucksack);
+            char actualOutlier = Day3.FindRucksackOutlier(rucksack);
             // Assert
 
-            Assert.AreEqual(expectedFirstCompartment, actualCompartments.First(), false);
-            Assert.AreEqual(expectedFirstCompartment, actualCompartments.Last(), false);
+            Assert.AreEqual(expectedOutlier, actualOutlier);
         }
     }
 }
