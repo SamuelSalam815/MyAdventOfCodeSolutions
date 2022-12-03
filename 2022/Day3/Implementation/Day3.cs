b2cb26efcbe0d3601c9fc60f83d98c89df2d3224
sam <samuelsalam815@gmail.com>
2022-12-03 07:37:34 +0000

    Implemented part 2

--- 2022/Day3/Implementation/Day3.cs
+++ 2022/Day3/Implementation/Day3.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿//#define IS_PART_1
+using System.Collections;
 
 public class Day3
 {
@@ -31,6 +32,7 @@ public class Day3
         return result;
     }
 
+
     public static char FindRucksackOutlier(string rucksack)
     {
         // Use bits to indicate the presence of an item in a compartment
@@ -41,7 +43,7 @@ public class Day3
         // Place all items into their comparments
         for(int i = 0; i < rucksack.Length; i++)
         {
-            if(i+1 < rucksack.Length / 2)
+            if(i < rucksack.Length / 2)
             {
                 firstCompartmentMask.Set(ToPriority(rucksack[i]) - 1, true);
             }
@@ -66,10 +68,62 @@ public class Day3
 
         throw new Exception("Unexpected failure to find outlier");
     }
+    public static char FindCommonChar(string rucksack1, string rucksack2, string rucksack3)
+    {
+        BitArray GetRucksackMask(string rucksack)
+        {
+            BitArray mask = new(52);
+            foreach(char item in rucksack)
+            {
+                mask.Set(ToPriority(item)-1, true);
+            }
+            return mask;
+        }
+        BitArray mask1 = GetRucksackMask(rucksack1);
+        BitArray mask2 = GetRucksackMask(rucksack2);
+        BitArray mask3 = GetRucksackMask(rucksack3);
+        
+        BitArray overlap = new(mask1);
+        overlap.And(mask2);
+        overlap.And(mask3);
+
+        for(int i = 0; i < overlap.Length; i++)
+        {
+            if (overlap.Get(i))
+            {
+                return FromPriority(i + 1);
+            }
+        }
+
+        throw new Exception("Unexpected failure to find common rucksack item");
+    }
 
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using StreamReader inputStream = new("input.txt");
+        string? line;
+        long totalPriority = 0;
+        long lineCounter = 0;
+
+#if IS_PART_1
+        while((line = inputStream.ReadLine()) is not null)
+        {
+            totalPriority += ToPriority(FindRucksackOutlier(line));
+            Console.Write($"{++lineCounter} Lines Processed ...\r");
+        }
+#else
+        while (inputStream.Peek() != -1)
+        {
+            string line1 = inputStream.ReadLine();
+            string line2 = inputStream.ReadLine();
+            string line3 = inputStream.ReadLine();
+            totalPriority += ToPriority(FindCommonChar(line1,line2,line3));
+            Console.Write($"{lineCounter += 3} Lines Processed ...\r");
+        }
+
+#endif
+        Console.WriteLine();
+        Console.WriteLine($"Total priority : {totalPriority}");
     }
 }
 
