86d643cf870f1ecb1a94eaee4b3170aedc6a6a98
sam <samuelsalam815@gmail.com>
2022-12-14 22:22:24 +0000

    Day 14

---
+++ 2022/Day14/Day14Solution.cs
@@ -0,0 +1,124 @@
+﻿using System.Diagnostics.SymbolStore;
+
+namespace Day14;
+
+internal class Day14Solution
+{
+        public record struct Position(int X, int Y);
+
+        static HashSet<Position> ConstructMap(List<RockStrucure> allRockStructures)
+        {
+                HashSet<Position> positionsWithRocks = new();
+
+                foreach (RockStrucure strucure in allRockStructures)
+                {
+                        foreach (Position rockPosition in strucure.GetAllRockPositions())
+                        {
+                                positionsWithRocks.Add(rockPosition);
+                        }
+                }
+
+                return positionsWithRocks;
+        }
+
+        // Returns true if sand settles and rockMap is updated (treat settled sand as a rock).
+        // Returns false if it drops into the void
+        static bool TrySettleSand(int sandX, int sandY, HashSet<Position> rockMap, int floorDepth = -1)
+        {
+                // Cannot settle sand if the source is blocked
+                if(rockMap.Contains(new Position(sandX, sandY)))
+                {
+                        return false;
+                }
+
+                bool floorExists = floorDepth > 0;
+                int maxDepth;
+
+                if (floorExists)
+                {
+                        maxDepth = floorDepth;
+                }
+                else
+                {
+                        maxDepth = rockMap.Select(pos => pos.Y).Max() + 1;
+                }
+
+                while(sandY + 1 < maxDepth)
+                {
+                        // Move down if there is a free space
+                        if (!rockMap.Contains(new Position(sandX, sandY + 1)))
+                        {
+                                sandY++;
+                                continue;
+                        }
+
+                        // Sand is blocked directly below; try to move diagonally down
+
+                        // First, consider moving left and down
+                        if (!rockMap.Contains(new Position(sandX - 1, sandY + 1))) // Left-Down is free and sand keeps falling
+                        {
+                                sandY++;
+                                sandX--;
+                                continue;
+                        }
+
+                        // Next, consider moving right and down
+                        if (!rockMap.Contains(new Position(sandX + 1, sandY + 1))) // Right-Down is free and sand keeps falling
+                        {
+                                sandX++;
+                                sandY++;
+                                continue;
+                        }
+
+                        // Sand is blocked, and settles in its current position
+                        rockMap.Add(new Position(sandX, sandY));
+                        return true;
+                }
+
+                if (floorExists)
+                {
+                        // Sand settles on the floor
+                        rockMap.Add(new Position(sandX, floorDepth-1));
+                        return true;
+                }
+                else
+                {
+                        // Sand fell past the lowest rock and never settles
+                        return false;
+                }
+        }
+
+        static void Main(string[] args)
+        {
+                const bool IS_FOR_PART_1 = false;
+
+
+                // Parse input into rock structures
+                using StreamReader input = new("input.txt");
+                List<RockStrucure> rockStrucures = new();
+                
+                while (!input.EndOfStream)
+                {
+                        rockStrucures.Add(RockStrucure.Parse(input.ReadLine()));
+                }
+
+                // Construct a map of all rock positions from the structures
+                HashSet<Position> rockMap = ConstructMap(rockStrucures);
+
+                int floorDepth;
+                if (IS_FOR_PART_1)
+                {
+                        floorDepth = -1;
+                }
+                else
+                {
+                        floorDepth = rockMap.Select(pos => pos.Y).Max() + 2;
+                }
+
+                // Simulate the falling sand
+                int numSandSettled;
+                for (numSandSettled = 0; TrySettleSand(500,0, rockMap, floorDepth); numSandSettled++) ;
+
+                Console.WriteLine(numSandSettled);
+        }
+}
---
+++ 2022/Day14/RockStrucure.cs
@@ -0,0 +1,61 @@
+﻿using static Day14.Day14Solution;
+
+namespace Day14
+{
+        internal class RockStrucure
+        {
+                // List of the endpoints of the straight lines of rock that make up the structure
+                readonly public List<Position> rockPathVertices;
+
+                public RockStrucure(List<Position> rockPathVertices)
+                {
+                        this.rockPathVertices = new List<Position>(rockPathVertices);
+                }
+
+                public List<Position> GetAllRockPositions()
+                {
+                        List<Position> allRocks = new();
+                        Position currentRock = rockPathVertices[0];
+                        int xStride;
+                        int yStride;
+                        foreach(Position lineEndpoint in rockPathVertices)
+                        {
+                                // Determine the direction to create this line of rocks
+                                xStride = 0;
+                                yStride = 0;
+                                if(currentRock.X != lineEndpoint.X)
+                                {
+                                        xStride = currentRock.X < lineEndpoint.X ? 1 : -1;
+                                }
+                                else
+                                {
+                                        yStride = currentRock.Y < lineEndpoint.Y ? 1 : -1;
+                                }
+
+                                // Add rocks until reaching the endpoint
+                                while(currentRock != lineEndpoint)
+                                {
+                                        allRocks.Add(currentRock);
+                                        currentRock = new Position(currentRock.X + xStride, currentRock.Y + yStride);
+                                }
+                        }
+                        allRocks.Add(currentRock);
+                        return allRocks;
+                }
+
+                public static RockStrucure Parse(string inputLine)
+                {
+                        List<Position> rockPathVertices = new();
+                        string[] endPointsAsCoordinates = inputLine.Split(" -> ");
+                        foreach (string coordinate in endPointsAsCoordinates)
+                        {
+                                string[] positions = coordinate.Split(',');
+                                int x = int.Parse(positions[0]);
+                                int y = int.Parse(positions[1]);
+                                rockPathVertices.Add(new Position(x, y));
+                        }
+
+                        return new RockStrucure(rockPathVertices);
+                }
+        }
+}
