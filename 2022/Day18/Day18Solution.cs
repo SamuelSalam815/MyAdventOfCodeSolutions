df6720e8b45905d8db6edb6d4f834017d99c59ce
sam <samuelsalam815@gmail.com>
2022-12-22 06:16:21 +0000

    Solved day 18 part 2

--- 2022/Day18/Day18Solution.cs
+++ 2022/Day18/Day18Solution.cs
@@ -1,6 +1,6 @@
 ﻿namespace Day18;
 
-internal class Day18Solution
+internal static class Day18Solution
 {
         private record struct Position(float X, float Y, float Z);
 
@@ -28,11 +28,11 @@ internal class Day18Solution
                         };
         }
 
-        static void Main(string[] args)
+        static void Main()
         {
-                HashSet<Position> surfacePositions = new();
+                HashSet<Position> lavaDropletSurfacePositions = new();
                 HashSet<Position> exposedPositions = new();
-                using StreamReader input = new("example.txt");
+                using StreamReader input = new("input.txt");
                 string? line;
 
                 // Define a bounding box that contains all droplets
@@ -46,7 +46,7 @@ internal class Day18Solution
                 float maxZ = float.MinValue;
 
                 // This function resizes our bounding box to be
-                // As small as possible with the given faces
+                // As small as possible for the given inputs
                 void UpdateBoundaryCoordinates(Position position)
                 {
                         minX = minX < position.X ? minX : position.X;
@@ -65,9 +65,9 @@ internal class Day18Solution
                         foreach(Position cubeFace in GetFaces(cubePosition))
                         {
                                 UpdateBoundaryCoordinates(cubePosition);
-                                if (!surfacePositions.Add(cubeFace))
+                                if (!lavaDropletSurfacePositions.Add(cubeFace))
                                 {
-                                        surfacePositions.Remove(cubeFace);
+                                        lavaDropletSurfacePositions.Remove(cubeFace);
                                 }
                         }
                 }
@@ -98,21 +98,52 @@ internal class Day18Solution
 
                 while(positionsToVisit.Count > 0)
                 {
-                        // TODO : perform a search on all free cubes
-                        // if the free cube has a face that is a surface, mark that surface as exposed
-                        // only expand on faces that are not surfaces
+                        Position currentCubePosition = positionsToVisit.Pop();
 
-                        bool TryExpandInDirection(float xStep, float yStep, float zStep)
+                        foreach(Position cubeFace in GetFaces(currentCubePosition))
                         {
-                                return false;
+                                // If the free cube has a face that is a surface, mark that surface as exposed
+                                if(lavaDropletSurfacePositions.Contains(cubeFace))
+                                {
+                                        exposedPositions.Add(cubeFace);
+                                        // Only expand on faces that are not surfaces
+                                        continue;
+                                }
+
+                                // Calculating the adjacent cube position, depending on the face position
+
+                                float xStride = cubeFace.X - currentCubePosition.X;
+                                float yStride = cubeFace.Y - currentCubePosition.Y;
+                                float zStride = cubeFace.Z - currentCubePosition.Z;
+
+                                Position adjacentFreeCube = new (cubeFace.X + xStride, cubeFace.Y + yStride, cubeFace.Z + zStride);
+
+                                // Do not expand to cubes out of bounds
+                                if( minX > adjacentFreeCube.X || maxX < adjacentFreeCube.X)
+                                {
+                                        continue;
+                                }
+                                if( minY > adjacentFreeCube.Y || maxY < adjacentFreeCube.Y)
+                                {
+                                        continue;
+                                }
+                                if( minZ > adjacentFreeCube.Z || maxZ < adjacentFreeCube.Z)
+                                {
+                                        continue;
+                                }
+
+                                // Do not expand to cubes we have already planned to visit
+                                if(seenPositions.Contains(adjacentFreeCube))
+                                {
+                                        continue;
+                                }
+
+                                seenPositions.Add(adjacentFreeCube);
+                                positionsToVisit.Push(adjacentFreeCube);
                         }
                 }
 
-                Console.WriteLine($" Total surface area : {surfacePositions.Count}");
-                Console.WriteLine($" Total EXPOSED surface area : {exposedPositions.Count}");
-        }
-        private static bool IsInRange(float valueToTest, float max, float min)
-        {
-                return min <= valueToTest && valueToTest <= max;
+                Console.WriteLine($"Total surface area : {lavaDropletSurfacePositions.Count}");
+                Console.WriteLine($"Total EXPOSED surface area : {exposedPositions.Count}");
         }
 }
