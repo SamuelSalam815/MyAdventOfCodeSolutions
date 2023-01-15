18ce8d1e9395b7eb1e9ab07663e706245e7831ae
sam <samuelsalam815@gmail.com>
2023-01-15 09:54:24 +0000

    Day 20 solved

--- 2022/Day20/CircularListNode.cs
+++ 2022/Day20/CircularListNode.cs
@@ -2,37 +2,83 @@ using System.Text;
 
 namespace Day20;
 
-internal class CircularListNode<T> where T : struct, IEquatable<T>
+internal class CircularListNode
 {
-    public T Value;
-    public CircularListNode<T>? Next;
-    public CircularListNode<T>? Previous;
+    public long Value;
+    public CircularListNode NextNode;
+    public CircularListNode PreviousNode;
 
-    public CircularListNode(
-        T value,
-        CircularListNode<T>? next = null,
-        CircularListNode<T>? previous = null
-    )
+
+    public CircularListNode(IList<long> nextValues) : this(nextValues.GetEnumerator(), null, null) { }
+
+    private CircularListNode(IEnumerator<long> ValueEnumerator, CircularListNode? PreviousNode, CircularListNode? FirstNode)
     {
-        Value = value;
-        Next = next;
-        Previous = previous;
+        bool thisIsFirstNode = PreviousNode is null;
+        if(thisIsFirstNode)
+        {
+            if(!ValueEnumerator.MoveNext())
+            {
+                throw new Exception("There must be at least one element in the collection");
+            }
+        }
+
+        Value = ValueEnumerator.Current;
+        if (ValueEnumerator.MoveNext())
+        {
+            NextNode = new(ValueEnumerator, this, FirstNode is null ? this : FirstNode);
+            
+            if(PreviousNode is not null)
+            {
+                this.PreviousNode = PreviousNode;
+            }
+            // If there is no previous value, then this is the first node
+            // Exploit wrap-around mechanic to find the last node
+            else
+            {
+                CircularListNode currentNode = NextNode;
+                while(currentNode.NextNode != this)
+                {
+                    currentNode = currentNode.NextNode;
+                }
+                this.PreviousNode = currentNode;
+            }
+        }
+        else // No values left, time to wrap-around
+        {
+            if(FirstNode is not null && PreviousNode is not null)
+            {
+                NextNode = FirstNode;
+                this.PreviousNode = PreviousNode;
+            }
+            else // singleton case
+            {
+                NextNode = this;
+                this.PreviousNode = this;
+            }
+
+        }
     }
 
-    public override string ToString()
+    /// <summary>
+    /// Prints the circular list, treating the node this is called on as the start of the list
+    /// </summary>
+    /// <returns>A string representation of the circular list</returns>
+    public string GetListAsString()
     {
         StringBuilder builder = new();
-        T firstValue = Value;
+        CircularListNode firstNode = this;
         builder.Append('[');
         builder.Append(Value);
-        CircularListNode<T> currentNode = this;
-        while (currentNode.Next?.Value.Equals(firstValue) == false)
+        CircularListNode currentNode = this;
+        while (currentNode.NextNode != firstNode)
         {
-            currentNode = currentNode.Next;
+            currentNode = currentNode.NextNode;
             builder.Append(", ");
             builder.Append(currentNode.Value);
         }
         builder.Append(']');
         return builder.ToString();
     }
+
+    public override string ToString() => Value.ToString();
 }
--- 2022/Day20/Program.cs
+++ 2022/Day20/Program.cs
@@ -2,9 +2,122 @@
 
 internal class Program
 {
+    private static List<long> parseInput(string filepath)
+    {
+        StreamReader reader = new(filepath);
+        List<long> inputList = new();
+        string? line;
+        while((line = reader.ReadLine()) is not null )
+        {
+            inputList.Add(long.Parse(line));
+        }
+        return inputList;
+    }
+
+    private static List<CircularListNode> GetOriginalNodeOrder(CircularListNode initialNode)
+    {
+        List<CircularListNode> originalNodeOrder = new();
+        CircularListNode currentNode = initialNode;
+        do
+        {
+            originalNodeOrder.Add(currentNode);
+            currentNode = currentNode.NextNode;
+        }
+        while (currentNode != initialNode);
+        return originalNodeOrder;
+    }
+
+    private static void MoveNode(CircularListNode targetNode, long stepsToTake)
+    {
+        while(stepsToTake != 0 )
+        {
+            CircularListNode originalPreviousNode = targetNode.PreviousNode;
+            CircularListNode originalNextNode = targetNode.NextNode;
+            CircularListNode newPreviousNode;
+            CircularListNode newNextNode;
+
+            if (stepsToTake > 0)
+            {
+                newPreviousNode = targetNode.NextNode;
+                newNextNode = targetNode.NextNode.NextNode;
+            }
+            else
+            {
+                newPreviousNode = targetNode.PreviousNode.PreviousNode;
+                newNextNode = targetNode.PreviousNode;
+            }
+
+            originalPreviousNode.NextNode = originalNextNode;
+            originalNextNode.PreviousNode = originalPreviousNode;
+
+            newPreviousNode.NextNode = targetNode;
+            newNextNode.PreviousNode = targetNode;
+
+            targetNode.PreviousNode = newPreviousNode;
+            targetNode.NextNode = newNextNode;
+
+            stepsToTake += stepsToTake > 0 ? -1 : 1;
+        }
+
+    }
+
+    private static long GetValueAtOffset(CircularListNode targetNode, uint offset)
+    {
+        for(int stepsTaken = 0; stepsTaken < offset; stepsTaken++)
+        {
+            targetNode = targetNode.NextNode;
+        }
+
+        return targetNode.Value;
+    }
+
     private static void Main(string[] args)
     {
-        CircularList<int> circularList = new(new List<int>() { 1, 2, -3, 3, -2, 0, 4 });
-        Console.WriteLine(circularList.GetNodeByValue(0));
+        const int decryptionKey = 811589153;
+        const int roundsOfMixing = 10;
+
+        CircularListNode firstNodeParsed = new(parseInput("input.txt"));
+        List<CircularListNode> originalNodeOrder = GetOriginalNodeOrder(firstNodeParsed);
+
+        foreach(var node in originalNodeOrder)
+        {
+            node.Value *= decryptionKey;
+        }
+        
+        List<CircularListNode> zeroNodes = originalNodeOrder.Where(x=>x.Value == 0).ToList();
+
+        if(zeroNodes.Count != 1)
+        {
+            throw new Exception($"Exactly 1 zero node is required. Found {zeroNodes.Count}");
+        }
+
+        CircularListNode zeroNode = zeroNodes[0];
+
+        //Console.WriteLine("Initial arrangement:");
+        //Console.WriteLine(originalNodeOrder[0].GetListAsString());
+
+        for(int i = 1; i <= roundsOfMixing; i++)
+        {
+            foreach(CircularListNode node in originalNodeOrder)
+            {
+                long stepsToTake = (Math.Abs(node.Value) ) % (originalNodeOrder.Count-1) * (node.Value > 0 ? 1 : -1 );
+                MoveNode(node, stepsToTake);
+            }
+            //Console.WriteLine();
+            //Console.WriteLine($"After {i} round{(i>1?"s":"")} of mixing:");
+            //Console.WriteLine(zeroNode.GetListAsString());
+        }
+
+        long val1 = GetValueAtOffset(zeroNode, 1000);
+        long val2 = GetValueAtOffset(zeroNode, 2000);
+        long val3 = GetValueAtOffset(zeroNode, 3000);
+
+        //Console.WriteLine();
+        //Console.WriteLine($"The 1000th number after 0 is {val1}");
+        //Console.WriteLine($"The 2000th number after 0 is {val2}");
+        //Console.WriteLine($"The 3000th number after 0 is {val3}");
+        //Console.WriteLine($"The sum of the three numbers that form the grove coordinates is {val1 + val2 + val3}");
+
+        Console.WriteLine(val1 + val2 + val3);
     }
 }
